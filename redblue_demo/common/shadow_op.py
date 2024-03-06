1d043731b44ebe6fe212cb144134dd2ffe4fb55b
Mengqi Guo <46805856+gmq0508@users.noreply.github.com>
2024-03-06 19:49:14 +0800

    fix: shadow_op.py

--- redblue_demo/common/shadow_op.py
+++ redblue_demo/common/shadow_op.py
@@ -22,8 +22,3 @@ class ShadowOp:
     def apply(self, bank: BankStorage) -> None:
         account = bank.get_account(self.aid)
         account.set_balance(account.get_balance() + self.amount)
-
-bank = BankStorage()
-shadow_op = ShadowOp(4, 5, 1, 10, COLOR.RED)
-shadow_op.apply(bank)
-print(bank.get_account(4).get_balance())
